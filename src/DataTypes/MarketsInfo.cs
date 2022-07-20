c235fea2543d051d6e161e8e849018475f1dae0d
Dmitry Safonov <d.safonov@glossgenius.com>
2022-07-20 09:30:43 -0400

    Wait for markets to be open.

---
+++ src/DataTypes/MarketsInfo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace PollQT.DataTypes
+{
+    internal class MarketsInfo : JsonSerializable<MarketsInfo>
+    {
+        public Market[] Markets { get; set; } = Array.Empty<Market>();
+    }
+
+    internal class Market : JsonSerializable<Market>
+    {
+        public string Name { get; set; } = "";
+        public DateTimeOffset StartTime { get; set; } = default;
+        public DateTimeOffset EndTime { get; set; } = default;
+    }
+}
--- src/Questrade/Client.cs
+++ src/Questrade/Client.cs
@@ -21,6 +21,7 @@ namespace PollQT.Questrade
         private HttpClient HttpClient { get; set; }
         private readonly ITokenStore tokenStore;
         private readonly ILogger log;
+        private Market? MarketInfo { get; set; }
 
         // the API sometimes throws 429 Too Many Requests if you go too fast
         private readonly IScheduler minimumRateLimiter;
@@ -32,20 +33,23 @@ namespace PollQT.Questrade
             ACCOUNT_BALANCES,
             ACCOUNT_POSITIONS,
             ACCOUNT_ACTIVITIES,
+            MARKETS,
         }
-        public Client(Context context) {
+        public Client(Context context)
+        {
             log = context.Logger.ForContext<Client>();
             HttpClient = new HttpClient();
             AuthToken = new Token();
             log.Information("workDir: {workDir}", context.WorkDir);
-            if (!Directory.Exists(context.WorkDir)) {
+            if (!Directory.Exists(context.WorkDir))
+            {
                 log.Information("Created {workDir}", context.WorkDir);
                 Directory.CreateDirectory(context.WorkDir);
             }
             tokenStore = new FileTokenStore(context);
             log.Information("tokenStore: {tokenStore}", tokenStore);
             NewToken(tokenStore.GetToken(), supressWrite: true);
-            minimumRateLimiter = new RateScheduler(context, 5, TimeSpan.FromSeconds(1)); 
+            minimumRateLimiter = new RateScheduler(context, 5, TimeSpan.FromSeconds(1));
         }
         private string RequestRoute(RequestType requestType, string? id = default) => requestType switch
         {
@@ -55,6 +59,7 @@ namespace PollQT.Questrade
             RequestType.ACCOUNT_BALANCES => $"v1/accounts/{id}/balances",
             RequestType.ACCOUNT_POSITIONS => $"v1/accounts/{id}/positions",
             RequestType.ACCOUNT_ACTIVITIES => $"v1/accounts/{id}/activities",
+            RequestType.MARKETS => "v1/markets",
             _ => throw new NotImplementedException()
         };
         private string RequestBaseAddress(RequestType requestType) => requestType switch
@@ -70,15 +75,18 @@ namespace PollQT.Questrade
             _ => ""
         };
 
-        private void NewToken(Token authToken, bool supressWrite = false) {
+        private void NewToken(Token authToken, bool supressWrite = false)
+        {
             AuthToken = authToken;
             HttpClient.DefaultRequestHeaders.Authorization = AuthenticationHeaderValue.Parse($"Bearer {authToken.AccessToken}");
-            if (!supressWrite) {
+            if (!supressWrite)
+            {
                 tokenStore.WriteToken(authToken);
             }
         }
 
-        private async Task<string> MakeRequest(RequestType type, CancellationToken cancelToken, string? id = default, string? queryOverride = default) {
+        private async Task<string> MakeRequest(RequestType type, CancellationToken cancelToken, string? id = default, string? queryOverride = default)
+        {
             minimumRateLimiter.WaitUntilRunnable();
 
             var stopwatch = Stopwatch.StartNew();
@@ -87,10 +95,13 @@ namespace PollQT.Questrade
             stopwatch.Stop();
             log.Debug("Request: {request}", resp.RequestMessage);
             log.Debug("Response: {resp}", resp);
-            if (!resp.IsSuccessStatusCode) {
-                if (resp.StatusCode == HttpStatusCode.Unauthorized) {
+            if (!resp.IsSuccessStatusCode)
+            {
+                if (resp.StatusCode == HttpStatusCode.Unauthorized)
+                {
                     // don't invoke any retry logic if we've been cancelled
-                    if (cancelToken.IsCancellationRequested) {
+                    if (cancelToken.IsCancellationRequested)
+                    {
                         throw new TaskCanceledException("MakeRequest Unauthorized Handler");
                     }
 
@@ -99,33 +110,45 @@ namespace PollQT.Questrade
                     await Login(cancelToken);
                     // Login will throw if it fails so we can retry here worry-free
                     return await MakeRequest(type, cancelToken, id);
-                } else if (resp.StatusCode == HttpStatusCode.TooManyRequests) {
-                    if (cancelToken.IsCancellationRequested) {
+                }
+                else if (resp.StatusCode == HttpStatusCode.TooManyRequests)
+                {
+                    if (cancelToken.IsCancellationRequested)
+                    {
                         throw new TaskCanceledException("MakeRequest TooManyRequests Handler");
                     }
 
-                    try {
+                    try
+                    {
                         var nextReset = DateTimeOffset.FromUnixTimeSeconds(long.Parse(resp.Headers.GetValues("X-RateLimit-Reset").First()));
                         minimumRateLimiter.RequestDelayUntil(nextReset);
                         log.Warning("Too Many Requests - waiting until rate reset at {dt}", nextReset);
                         return await MakeRequest(type, cancelToken, id);
-                    } catch (Exception e) {
+                    }
+                    catch (Exception e)
+                    {
                         throw new ApiException("Told TooManyRequests but not told when we can make more", e);
                     }
-                } else {
+                }
+                else
+                {
                     log.Error("Error: {type} {resp} ({dur}ms)", type, resp, stopwatch.ElapsedMilliseconds);
                     throw resp.StatusCode switch
                     {
                         _ => new UnexpectedStatusException(resp),
                     };
                 }
-            } else {
+            }
+            else
+            {
                 log.Information("Success: {type} ({dur}ms)", type, stopwatch.ElapsedMilliseconds);
                 return await resp.Content.ReadAsStringAsync();
             }
         }
-        private async Task Login(CancellationToken cancelToken) {
-            if (cancelToken.IsCancellationRequested) {
+        private async Task Login(CancellationToken cancelToken)
+        {
+            if (cancelToken.IsCancellationRequested)
+            {
                 throw new TaskCanceledException("Login");
             }
 
@@ -136,8 +159,10 @@ namespace PollQT.Questrade
             NewToken(Token.FromJson(authToken));
             log.Information("Successfully logged in with refresh token");
         }
-        private async Task<T> GetResponse<T>(RequestType type, CancellationToken cancelToken, string? id = default) where T : JsonSerializable<T> {
-            if (cancelToken.IsCancellationRequested) {
+        private async Task<T> GetResponse<T>(RequestType type, CancellationToken cancelToken, string? id = default) where T : JsonSerializable<T>
+        {
+            if (cancelToken.IsCancellationRequested)
+            {
                 throw new TaskCanceledException("GetResponse");
             }
 
@@ -147,14 +172,17 @@ namespace PollQT.Questrade
             log.Debug("{resName}: {@res}", resObj.GetType(), resObj);
             return resObj;
         }
-        private async Task<List<PollResult>> Poll(CancellationToken cancelToken) {
-            if (AuthToken.ApiServer is null || AuthToken.AccessToken is null) {
+        private async Task<List<PollResult>> Poll(CancellationToken cancelToken)
+        {
+            if (AuthToken.ApiServer is null || AuthToken.AccessToken is null)
+            {
                 await Login(cancelToken);
             }
             var accounts = await GetResponse<AccountsResponse>(RequestType.ACCOUNTS, cancelToken);
             var timestamp = DateTimeOffset.UtcNow;
             var resp = new List<PollResult>();
-            foreach (var account in accounts.Accounts) {
+            foreach (var account in accounts.Accounts)
+            {
                 var accountBalances = await GetResponse<AccountBalancesResponse>(RequestType.ACCOUNT_BALANCES, cancelToken, account.Number);
                 var accountPositions = await GetResponse<AccountPositionsResponse>(RequestType.ACCOUNT_POSITIONS, cancelToken, account.Number);
                 var accountActivities = await GetResponse<AccountActivitiesResponse>(RequestType.ACCOUNT_ACTIVITIES, cancelToken, account.Number);
@@ -163,14 +191,17 @@ namespace PollQT.Questrade
             return resp;
         }
 
-        public async Task<List<(Account account, List<AccountActivity> activities)>> BackfillActivities(CancellationToken cancelToken) {
+        public async Task<List<(Account account, List<AccountActivity> activities)>> BackfillActivities(CancellationToken cancelToken)
+        {
             await Login(cancelToken);
             var accounts = await GetResponse<AccountsResponse>(RequestType.ACCOUNTS, cancelToken);
             var resp = new List<(Account account, List<AccountActivity>)>();
-            foreach (var account in accounts.Accounts) {
+            foreach (var account in accounts.Accounts)
+            {
                 var activities = new List<AccountActivity>();
                 var searchStart = new DateTimeOffset(DateTime.Today.Year - 5, 1, 1, 0, 0, 0, 0, TimeSpan.Zero);
-                while (searchStart < DateTimeOffset.Now) {
+                while (searchStart < DateTimeOffset.Now)
+                {
                     var searchEnd = searchStart.AddMonths(1).AddDays(-1);
                     var query = $"?startTime={searchStart:o}&endTime={searchEnd:o}";
                     var queryRes = await MakeRequest(RequestType.ACCOUNT_ACTIVITIES, cancelToken, account.Number, queryOverride: query);
@@ -184,34 +215,93 @@ namespace PollQT.Questrade
             return resp;
         }
 
-        public async Task<List<PollResult>> PollWithRetry(int maxRetries = 10) {
+        public async Task<bool> MarketOpenDelay(CancellationToken token)
+        {
+            if (MarketInfo == null || MarketInfo.StartTime.Date < DateTimeOffset.Now.Date)
+            {
+                var marketsResponse = await GetResponse<MarketsInfo>(RequestType.MARKETS, token);
+                MarketInfo = marketsResponse.Markets.Where(m => m.Name == "TSX").First();
+                if (MarketInfo.StartTime.Date < DateTimeOffset.Now.Date)
+                {
+                    var startTimeToday = DateTimeOffset.Now.Date + (MarketInfo.StartTime - MarketInfo.StartTime.Date);
+                    if (startTimeToday > DateTimeOffset.Now)
+                    {
+                        var ts = startTimeToday - DateTimeOffset.Now;
+                        log.Information($"Market start time is {MarketInfo.StartTime} on {DateTimeOffset.Now}, waiting until start time today: {ts}");
+                        await Task.Delay(ts);
+                        return false;
+                    }
+                    else
+                    {
+                        var ts = startTimeToday.AddDays(1) - DateTimeOffset.Now;
+                        log.Information($"Market start time is {MarketInfo.StartTime} on {DateTimeOffset.Now}, waiting until start time tomorrow: {ts}");
+                        await Task.Delay(ts);
+                        return false;
+                    }
+                }
+            }
+
+            if (DateTimeOffset.Now < MarketInfo.StartTime)
+            {
+                var ts = MarketInfo.StartTime - DateTimeOffset.Now;
+                log.Information($"Waiting for market open: {MarketInfo.StartTime}, {ts}");
+                await Task.Delay(ts, token);
+                return true;
+            }
+            else if (DateTimeOffset.Now > MarketInfo.EndTime)
+            {
+                var ts = MarketInfo.StartTime.AddDays(1) - DateTimeOffset.Now;
+                log.Information($"Waiting until tomorrow, {ts}");
+                await Task.Delay(ts, token);
+                return false;
+            }
+
+            return true;
+        }
+
+        public async Task<List<PollResult>> PollWithRetry(int maxRetries = 10)
+        {
             var backoff = new ExponentialBackoff(maxRetries, delayMilliseconds: 200, maxDelayMilliseconds: 120000);
+
         retry:
             var cts = new CancellationTokenSource();
+            Console.CancelKeyPress += delegate { cts.Cancel(); };
+            while (!await MarketOpenDelay(cts.Token)) ;
             var awaitable = Poll(cts.Token);
-            try {
+            try
+            {
                 var result = await awaitable.TimeoutAfter(TimeSpan.FromSeconds(60));
                 return result;
-            } catch (Exception e) {
-                if (e is ApiException) {
+            }
+            catch (Exception e)
+            {
+                if (e is ApiException)
+                {
                     log.Error(e, "API issue - backing off to retry in {delay} ms.", backoff.NextDelay);
                     await backoff.Delay();
                     goto retry;
-                } else if (e is TimeoutException) {
+                }
+                else if (e is TimeoutException)
+                {
                     cts.Cancel(); // signal cancellation -- mainly so we don't mess with stored tokens out-of-band
 
                     // Continue tracking it on the thread pool but abandon the results
-                    _ = awaitable.ContinueWith((t) => {
+                    _ = awaitable.ContinueWith((t) =>
+                    {
                         log.Warning(t.Exception, "Information from task that was abandoned due to timeout: Status {status} Result {result}", t.Status, t.Result);
                     });
 
                     log.Error(e, "Timed out - backing off to retry in {delay} ms.", backoff.NextDelay);
                     await backoff.Delay();
                     goto retry;
-                } else {
+                }
+                else
+                {
                     throw;
                 }
-            } finally {
+            }
+            finally
+            {
                 cts.Dispose();
             }
         }
